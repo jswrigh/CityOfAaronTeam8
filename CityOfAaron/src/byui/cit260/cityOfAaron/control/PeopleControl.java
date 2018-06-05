265a2e592f87307a7fcb3ff20412e461f5f90293
Charlot <35281633+garraud@users.noreply.github.com>
2018-06-05 00:32:17 -0400

    Merge origin/master
    Conflicts:
    	CityOfAaron/src/byui/cit260/cityOfAaron/model/Point.java

--- CityOfAaron/src/byui/cit260/cityOfAaron/control/PeopleControl.java
+++ CityOfAaron/src/byui/cit260/cityOfAaron/control/PeopleControl.java
@@ -11,4 +11,15 @@ package byui.cit260.cityOfAaron.control;
  */
 public class PeopleControl {
     
-}
+    public static int shrinkPopulation (int foodBushels, int BUSHELS_PER_PERSON, int currentPopulation) {
+           
+           int peopleFed = foodBushels/BUSHELS_PER_PERSON;           
+           int peopleStarved = currentPopulation - peopleFed;
+            if(peopleStarved > 0){ 
+             return currentPopulation-peopleStarved;
+            }
+            else {
+                return 0;
+            }
+          }
+        }
