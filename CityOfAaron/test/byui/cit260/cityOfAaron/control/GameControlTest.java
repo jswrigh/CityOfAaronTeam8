59ca43a5674dee51f17c8ed234f6ac6653d42902
Sterling Wright <j.sterling.wright@gmail.com>
2018-06-16 14:29:09 -0400

    6/16/18 - Improved GameControl and test liveTheYear() still needs to be written.

--- CityOfAaron/test/byui/cit260/cityOfAaron/control/GameControlTest.java
+++ CityOfAaron/test/byui/cit260/cityOfAaron/control/GameControlTest.java
@@ -148,11 +148,9 @@ public class GameControlTest {
     @Test
     public void testLiveTheYear() {
         System.out.println("liveTheYear");
-        int expResult = 0;
+        int expResult = 0; // I know this doesn't work yet.
         int result = GameControl.liveTheYear();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
