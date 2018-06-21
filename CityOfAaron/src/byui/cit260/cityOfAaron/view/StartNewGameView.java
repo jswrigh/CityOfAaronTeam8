163dd7872b597d09a7d95a1be5cf71baaacadb83
Sterling Wright <j.sterling.wright@gmail.com>
2018-06-20 21:53:25 -0400

    6/20/18 - Refactored StartNewGameView

--- CityOfAaron/src/byui/cit260/cityOfAaron/view/StartNewGameView.java
+++ CityOfAaron/src/byui/cit260/cityOfAaron/view/StartNewGameView.java
@@ -10,10 +10,8 @@ import byui.cit260.cityOfAaron.view.GameMenuView;
  *
  * @author kanderson
  */
-public class StartNewGameView {
-    
-    
-    /**
+public class StartNewGameView extends ViewBase {   
+     /**
      * The message that will be displayed by this view.
      */
     protected String message;
@@ -22,58 +20,13 @@ public class StartNewGameView {
      * Constructor
      */
     public StartNewGameView(){
-        message = " ";
-    }
-    
-    
-    /**
-     * Get the user's input. Keep prompting them until they enter a value.
-     * @param prompt
-     * @param allowEmpty - determine whether the user can enter no value (just a return key)
-     * @return 
-     */
-    protected String getUserInput(String prompt, boolean allowEmpty){
-        
-        Scanner keyboard = new Scanner(System.in);
-        String input = "";
-        boolean inputReceived = false;
-        
-        while(inputReceived == false){
-            
-            System.out.println(prompt);
-            input = keyboard.nextLine();
-            
-            // Make sure we avoid a null-pointer error.
-            if (input == null){
-                input = "";
-            }
-            
-            // Trim any trailing whitespace, including the carriage return.
-            input = input.trim();
-            
-            if (input.equals("") == false || allowEmpty == true){
-                inputReceived = true;
-            }
-        }
-        
-        return input;
-    }
-    
-    
-    /**
-     * An overloaded version of getUserInput that sets allowEmpty to false so we don't have 
-     * to type it ourselves.
-     * @param prompt
-     * @return 
-     */
-    protected String getUserInput(String prompt){
-        return getUserInput(prompt, false);
-    }
+    }    
     
     /**
      * Get the set of inputs from the user.
      * @return 
      */
+    @Override
     public String[] getInputs() {
         
         // Declare the array to have the number of elements you intend to get 
@@ -94,6 +47,7 @@ public class StartNewGameView {
      * @return true if the view should repeat itself, and false if the view
      * should exit and return to the previous view.
      */
+    @Override
     public boolean doAction(String[] inputs){
         
         // If the user just hit Enter then bail out without doing the action
@@ -107,25 +61,6 @@ public class StartNewGameView {
         
         return false;
     }
-    
-    
-    /**
-     * Control this view's display/prompt/action loop until the user
-     * chooses and action that causes this view to close.
-     */
-    public void displayView(){
-        
-        boolean keepGoing = true;
-        
-        while(keepGoing == true){
-            
-            System.out.println(message);
-            String[] inputs = getInputs();
-            keepGoing = doAction(inputs);
-        }
-    }
-    
-    
     // Define your action handlers here. These are the methods that your doAction()
     // method will call based on the user's input. We don't want to do a lot of 
     // complex game stuff in our doAction() method. It will get messy very quickly.
@@ -150,5 +85,9 @@ public class StartNewGameView {
         
         GameMenuView gameMenuView = new GameMenuView();
         gameMenuView.displayView();
+    }
+
+    @Override
+    protected String getMessage() {
     }
 }
