ee8d052db6ecc0ec627f52f5deb3f3c1a7dccccd
Sterling Wright <j.sterling.wright@gmail.com>
2018-06-28 18:04:04 -0400

    6/28/18 - Added imports and stubs for assignment

--- CityOfAaron/src/byui/cit260/cityOfAaron/view/StartNewGameView.java
+++ CityOfAaron/src/byui/cit260/cityOfAaron/view/StartNewGameView.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 import byui.cit260.cityOfAaron.model.*;
 import cityofaaron.*;
 import byui.cit260.cityOfAaron.view.GameMenuView;
+import byui.cit260.cityOfAaron.control.GameControl;
+
 
 /**
  *
@@ -77,10 +79,14 @@ public class StartNewGameView extends ViewBase {
         Player player = new Player();
         player.setName(playerName);
         
+        /* Old code that made it work earlier
         Game game = new Game();
         game.setThePlayer(player);
         
         CityOfAaron.setCurrentGame(game);
+        */
+    // Now we're ready to make the call as outlined in comments at top of this method.    
+        Game game = GameControl.createNewGame(playerName);
         
         System.out.println("");
         System.out.println("Welcome to the game," + CityOfAaron.getCurrentGame().getThePlayer().getName() + "!\n");
